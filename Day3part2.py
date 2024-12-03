42668948a994e79169975d9ff6fb8d06c6deb7e7
Matevž Rom <i-matevz.rom@devrev.ai>
2024-12-03 10:34:34 +0100

    Add Day3part2

---
+++ Day3part2.py
@@ -0,0 +1,44 @@
+def zmnozi(text, naredi):
+    i = 0
+    cont = False
+    zmnozek = 0
+    while i < len(text):
+        if text[i:i+4] == "do()":
+            naredi = True
+        if text[i:i+7] == "don't()":
+            naredi = False
+        if text[i:i+3] == "mul" and naredi:
+            i=i+3
+            if text[i]=="(":
+                number1 = ""
+                number2 = ""
+                j = 1
+                first = True
+                while True:
+                    if text[i+j].isdigit() and first:
+                        number1 += text[i+j]
+                    elif text[i+j]==',':
+                        first = False
+                    elif text[i+j].isdigit() and not first:
+                        number2 += text[i+j]
+                    elif text[i+j]==')' and number1 != "" and number2 != "":
+                        zmnozek += int(number1)*int(number2)
+                        break
+                    else:
+                        cont = True
+                        break
+                    j+=1
+                if cont:
+                    continue
+        i += 1
+    return zmnozek, naredi
+
+f = open("input.txt", "r")
+text = ""
+skupniZmnozek = 0
+naredi = True
+for line in f:
+    zmnozek,naredi = zmnozi(line, naredi)
+    skupniZmnozek+= zmnozek
+print(skupniZmnozek)
+
